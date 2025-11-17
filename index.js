ebe7533b732bb6a6ea23dfb54252279a640433b4
jjjamil <jamiljoseph25@gmail.com>
2025-11-17 13:33:17 +0800

    Add Carousel and Lazy load images

--- index.js
+++ index.js
@@ -134,3 +134,25 @@ window.addEventListener('scroll', () => {
     animateCounter('viewsCount', 8, 5000);
   }
 });
+
+//swiper
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: { slidesPerView: 1, spaceBetween: 10 },
+      576: { slidesPerView: 1, spaceBetween: 10 },
+      768: { slidesPerView: 2, spaceBetween: 20 },
+      992: { slidesPerView: 3, spaceBetween: 30 },
+    },
+  });
